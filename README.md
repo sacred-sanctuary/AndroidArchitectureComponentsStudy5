258af6ef74a239b2d812bf6f6cf60897ca4c55fb
Yuichi Yagami <yagami@setram.jp>
2019-03-29 19:00:00 +0900

    Android Architecture Components Study 5 NO.1.1

--- README.md
+++ README.md
@@ -3,5 +3,64 @@
 [Android Architecture Components 勉強会 #5](https://gdg-tokyo.connpass.com/event/123068/)  
 での課題取り組みの実装例
 
-## 課題0.2
-app/build.gradleの設定
+## 課題1.1
+Activityの各ライフサイクルメソッドでLifecycleのcurrentStateを確認する
+
+
+## Activityの起動時
+```
+I/System.out: onCreate : INITIALIZED
+I/System.out: onStart : CREATED
+I/System.out: onResume : STARTED
+```
+
+## Back KeyによるActivityの終了時
+```
+I/System.out: onPause : STARTED
+I/System.out: onStop : CREATED
+I/System.out: onDestroy : DESTROYED
+```
+
+## Home KeyによるHome App表示時
+```
+I/System.out: onPause : STARTED
+I/System.out: onStop : CREATED
+I/System.out: onSaveInstanceState : CREATED
+```
+
+## Recents KeyによるRecent Appの表示時
+```
+I/System.out: onPause : STARTED
+I/System.out: onStop : CREATED
+I/System.out: onSaveInstanceState : CREATED
+```
+
+## Recent AppからActivityの起動時(Activityは終了済み)
+```
+I/System.out: onCreate : INITIALIZED
+I/System.out: onStart : CREATED
+I/System.out: onResume : STARTED
+```
+
+## Recent AppからActivityの起動時(ActivityはStop中)
+```
+I/System.out: onRestart : CREATED
+I/System.out: onStart : CREATED
+I/System.out: onResume : STARTED
+```
+
+## Recent AppからActivity削除時(ActivityはStop中)
+```
+I/System.out: onDestroy : DESTROYED
+```
+
+## Multi-Window ModeでActivityのフォーカスが外れた時
+```
+I/System.out: onPause : STARTED
+```
+
+## Multi-Window ModeでActivityがフォーカスを得た時
+```
+I/System.out: onResume : STARTED
+```
+
--- app/src/main/java/jp/sacredsanctuary/androidarchitecturecomponentsstudy/MainActivity.kt
+++ app/src/main/java/jp/sacredsanctuary/androidarchitecturecomponentsstudy/MainActivity.kt
@@ -1,12 +1,90 @@
 package jp.sacredsanctuary.androidarchitecturecomponentsstudy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * {@inheritDoc}
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("onCreate : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onStart() {
+        super.onStart()
+
+        println("onStart : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onRestart() {
+        super.onRestart();
+
+        println("onRestart : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onResume() {
+        super.onResume()
+
+        println("onResume : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        println("onNewIntent : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        println("onSaveInstanceState : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onPause() {
+        super.onPause()
+
+        println("onPause : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onStop() {
+        super.onStop()
+
+        println("onStop : ${lifecycle.currentState.name}")
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+
+        println("onDestroy : ${lifecycle.currentState.name}")
     }
 }
