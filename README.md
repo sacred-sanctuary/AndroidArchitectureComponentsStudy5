f6c885516919d8e8e3f5de4cf029463610e5b027
Yuichi Yagami <yagami@setram.jp>
2019-04-04 17:47:19 +0900

    Android Architecture Components Study 5 NO.2.3

--- README.md
+++ README.md
@@ -3,5 +3,73 @@
 [Android Architecture Components 勉強会 #5](https://gdg-tokyo.connpass.com/event/123068/)  
 での課題取り組みの実装例
 
-## 課題0.2
-app/build.gradleの設定
+## 課題2.3
+ON_STOP時にobserverを解除すると、ON_DESTROY時のメソッドが呼ばれなくなるのを確認する
+
+## Activityの起動時
+```
+I/System.out: ON_CREATE : CREATED
+I/System.out: ON_ANY : CREATED
+I/System.out: ON_START : STARTED
+I/System.out: ON_ANY : STARTED
+I/System.out: ON_RESUME : RESUMED
+I/System.out: ON_ANY : RESUMED
+```
+
+## Back KeyによるActivityの終了時
+```
+I/System.out: ON_PAUSE : STARTED
+I/System.out: ON_ANY : STARTED
+I/System.out: ON_STOP : CREATED
+I/System.out: ON_ANY : CREATED
+
+```
+
+## Home KeyによるHome App表示時
+```
+I/System.out: ON_PAUSE : STARTED
+I/System.out: ON_ANY : STARTED
+I/System.out: ON_STOP : CREATED
+I/System.out: ON_ANY : CREATED
+```
+
+## Recents KeyによるRecent Appの表示時
+```
+I/System.out: ON_PAUSE : STARTED
+I/System.out: ON_ANY : STARTED
+I/System.out: ON_STOP : CREATED
+I/System.out: ON_ANY : CREATED
+```
+
+## Recent AppからActivityの起動時(Activityは終了済み)
+```
+I/System.out: ON_CREATE : CREATED
+I/System.out: ON_ANY : CREATED
+I/System.out: ON_START : STARTED
+I/System.out: ON_ANY : STARTED
+I/System.out: ON_RESUME : RESUMED
+I/System.out: ON_ANY : RESUMED
+```
+
+## Recent AppからActivityの起動時(ActivityはStop中)
+```
+Nothing
+```
+
+## Recent AppからActivity削除時(ActivityはStop中)
+```
+Nothing
+```
+
+## Multi-Window ModeでActivityのフォーカスが外れた時
+```
+I/System.out: ON_PAUSE : STARTED
+I/System.out: ON_ANY : STARTED
+```
+
+## Multi-Window ModeでActivityがフォーカスを得た時
+```
+I/System.out: ON_RESUME : RESUMED
+I/System.out: ON_ANY : RESUMED
+```
+
--- app/src/main/java/jp/sacredsanctuary/androidarchitecturecomponentsstudy/MainActivity.kt
+++ app/src/main/java/jp/sacredsanctuary/androidarchitecturecomponentsstudy/MainActivity.kt
@@ -2,11 +2,51 @@ package jp.sacredsanctuary.androidarchitecturecomponentsstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * {@inheritDoc}
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lifecycleObserver = object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate(source: LifecycleOwner) {
+                println("ON_CREATE : ${source.lifecycle.currentState.name}")
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onStart(source: LifecycleOwner) {
+                println("ON_START : ${source.lifecycle.currentState.name}")
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume(source: LifecycleOwner) {
+                println("ON_RESUME : ${source.lifecycle.currentState.name}")
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause(source: LifecycleOwner) {
+                println("ON_PAUSE : ${source.lifecycle.currentState.name}")
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStop(source: LifecycleOwner) {
+                println("ON_STOP : ${source.lifecycle.currentState.name}")
+                source.lifecycle.removeObserver(this)
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy(source : LifecycleOwner) {
+                println("ON_DESTROY : ${source.lifecycle.currentState.name}")
+            }
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAny(source: LifecycleOwner, event: Lifecycle.Event) {
+                println("ON_ANY : ${source.lifecycle.currentState.name}")
+            }
+        }
+        lifecycle.addObserver(lifecycleObserver)
     }
 }
